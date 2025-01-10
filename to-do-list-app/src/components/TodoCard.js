566a0c2ce1c1ac7e32723d2d7bc18647d27d28a6
gurvir99 <67031616+gurvir99@users.noreply.github.com>
2025-01-09 23:43:06 -0500

    added icons to modify and remove tasks.

--- to-do-list-app/src/components/TodoCard.js
+++ to-do-list-app/src/components/TodoCard.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function TodoCard() {
-  return (
-    <div>TodoCard</div>
-  )
+export default function TodoCard(props) {
+    const {children} = props
+    return (
+       <li className='todoItem'>
+            {children}
+            <div className='actionsContainer'>
+               <i className="fa-solid fa-file-pen"></i>
+               <i className="fa-solid fa-xmark"></i>
+            </div>
+        </li>
+    )
 }
--- to-do-list-app/src/components/TodoList.js
+++ to-do-list-app/src/components/TodoList.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
+
+  let tasks = [
+    'Watch The Substance',
+    'Clean room',
+    'Study'
+  ]
+
   return (
-    <div>TodoList</div>
+    <ul className='main'>
+      {tasks.map((task, taskIndex) => {
+        return (
+          <TodoCard key={taskIndex}>
+            <p>{task}</p>
+          </TodoCard>
+      )
+      })}
+    </ul>
   )
 }
